a58cc4cc8b07f1849699a594d0ad3d48305a605e
Miqdad <miqdad.shaldin@gmail.com>
2025-06-07 11:40:29 +0700

    fetch data from API and render it to browser page

--- script.js
+++ script.js
@@ -16,8 +16,27 @@ async function fetchData(){
         const cardContainer = document.querySelector(".card-container");
         const res = await fetch(url);
         const forecast = await res.json();
-        console.log(res);
         console.log(forecast);
+
+        for(let i = 0; i < forecast.daily.time.length; i++){
+            const newList = `
+            <div class="card">
+          <h4>${forecast.daily.time[i]}</h4>
+          <img
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRSyCftnvcb2Sqg4hg7wzqpNbyf48WEBWWQsQ&s"
+            alt=""
+          />
+          <p class="suhu">${forecast.daily.temperature_2m_min[i]}&deg;C</p>
+          <p class="weather-name">Cerah Berawan</p>
+          <div class="card-box">
+            <p>💦 <span>79%</span></p>
+            <p>💨 <span>2km/jam</span></p>
+            <p>🧭 <span>Barat laut</span></p>
+            <p>👀 <span>> 10 km</span></p>
+          </div>
+        </div>`;
+        cardContainer.innerHTML += newList;
+        }
     } catch (error) {
         console.log(error)
     }
