ab8a0c2b35e1e185745f7e8337b3765d7e7eb604
Miqdad <miqdad.shaldin@gmail.com>
2025-06-07 11:59:29 +0700

    add button and box head from fetching data, create 2 more format date

--- script.js
+++ script.js
@@ -11,17 +11,57 @@ function formatDate(date){
     return result;
 }
 
+function formatDate2(date){
+    const options = {
+        year: "numeric",
+        month: "long",
+        day: "numeric"
+    };
+    let result = new Date(date).toLocaleDateString("id-ID", options);
+    return result;
+}
+
+function formatDate3(date){
+    const options = {
+        month: "long",
+        day: "numeric"
+    };
+    let result = new Date(date).toLocaleDateString("id-ID", options);
+    return result;
+}
+
 async function fetchData(){
     try {
+        const boxHead = document.querySelector(".box-head");
         const cardContainer = document.querySelector(".card-container");
         const res = await fetch(url);
         const forecast = await res.json();
-        console.log(forecast);
+        // see fetch data
+        // console.log(forecast);
+        const headerList = `
+        <p class="waktu">${formatDate(forecast.daily.time[0])}</p>
+          <div class="box-suhu">
+            <p>${forecast.daily.temperature_2m_min[0]}&deg;C</p>
+            <p>Cerah Berawan <span>.</span></p>
+            <p>di Jakarta</p>
+          </div>
+          <div class="box-legend">
+            <p>💦 Kelembapan: <span>79%</span></p>
+            <p>💨 Kecepatan angin: <span>2km/jam</span></p>
+            <p>🧭 Arah angin dari: <span>Barat laut</span></p>
+            <p>👀 Jarak pandang: <span>> 10 km</span></p>
+          </div>`;
+        boxHead.innerHTML += headerList;
 
         for(let i = 0; i < forecast.daily.time.length; i++){
+            // date button items
+            const buttonBox = document.querySelector(".button-box")
+            const buttonList = `<button>${formatDate3(forecast.daily.time[i])}</button>`
+            buttonBox.innerHTML += buttonList;
+            // card items
             const newList = `
             <div class="card">
-          <h4>${forecast.daily.time[i]}</h4>
+          <h4>${formatDate2(forecast.daily.time[i])}</h4>
           <img
             src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRSyCftnvcb2Sqg4hg7wzqpNbyf48WEBWWQsQ&s"
             alt=""
